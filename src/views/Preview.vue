926d0d0354c4f9285eac2defa06e14ddac392ea7
Aayush Pal <ap.aayushpal@gmail.com>
2019-08-06 11:53:49 +0530

    added price section

--- src/views/Preview.vue
+++ src/views/Preview.vue
@@ -9,60 +9,65 @@
       <v-btn text @click="logout">Log out</v-btn>
     </v-toolbar-items>
   </v-toolbar>
+    <v-layout align-content-space-around justify-start row>
+      <v-flex class="view" xs12 sm12 md6 lg3 xl3 d-flex>
+        <v-select
+          :items="items"
+          label="Solo field"
+          solo
+        ></v-select>
+        <v-select
+          :items="items"
+          label="Solo field"
+          solo
+        ></v-select>
+      </v-flex>
+    </v-layout>
+     <v-layout align-content-space-around justify-start row>
+      <v-flex class="view" xs12 sm12 md6 lg3 xl3 d-flex>
+        <v-select
+          :items="items"
+          label="Solo field"
+          solo
+        ></v-select>
+        <v-select
+          :items="items"
+          label="Solo field"
+          solo
+        ></v-select>
+      </v-flex>
+    </v-layout>
+    <v-layout align-content-space-around justify-start row>
+      <v-flex class="view" xs12 sm12 md6 lg3 xl3 d-flex>
+        <v-select
+          :items="items"
+          label="Solo field"
+          solo
+        ></v-select>
+        <v-select
+          :items="items"
+          label="Solo field"
+          solo
+        ></v-select>
+      </v-flex>
+    </v-layout>
+    <v-layout align-content-space-around justify-start row>
+      <v-flex class="view" xs12 sm12 md6 lg3 xl3 d-flex>
+        <v-select
+          :items="items"
+          label="Solo field"
+          solo
+        ></v-select>
+        <v-select
+          :items="items"
+          label="Solo field"
+          solo
+        ></v-select>
+      </v-flex>
+    </v-layout>
     <v-layout align-center justify-start>
-      <v-flex class="view" xs12 sm12 md6 lg3 xl3 d-flex>
-        <v-select
-          :items="items"
-          label="Solo field"
-          solo
-        ></v-select>
-        <v-select
-          :items="items"
-          label="Solo field"
-          solo
-        ></v-select>
-      </v-flex>
-    </v-layout>
-     <v-layout align-content-space-around justify-start>
-      <v-flex class="view" xs12 sm12 md6 lg3 xl3 d-flex>
-        <v-select
-          :items="items"
-          label="Solo field"
-          solo
-        ></v-select>
-        <v-select
-          :items="items"
-          label="Solo field"
-          solo
-        ></v-select>
-      </v-flex>
-    </v-layout>
-    <v-layout align-content-space-around justify-start>
-      <v-flex class="view" xs12 sm12 md6 lg3 xl3 d-flex>
-        <v-select
-          :items="items"
-          label="Solo field"
-          solo
-        ></v-select>
-        <v-select
-          :items="items"
-          label="Solo field"
-          solo
-        ></v-select>
-      </v-flex>
-    </v-layout>
-    <v-layout align-content-space-around justify-start>
-      <v-flex class="view" xs12 sm12 md6 lg3 xl3 d-flex>
-        <v-select
-          :items="items"
-          label="Solo field"
-          solo
-        ></v-select>
-        <v-select
-          :items="items"
-          label="Solo field"
-          solo
-        ></v-select>
+      <v-flex offset-xs5 offset-sm5 offset-md6 offset-lg1 xl6>
+      <h4>Price:</h4>
       </v-flex>
     </v-layout>
 </v-app>
@@ -89,6 +94,9 @@ export default {
 <style>
 .view{
   padding: 30px;
-  max-height: 20px
+  height: 20px
+};
+.showprice{
+  padding: 40px 
 }
 </style>
